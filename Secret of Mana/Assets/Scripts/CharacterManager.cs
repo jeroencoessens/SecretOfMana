3daef3f94d004b8b232cdd4a351ac36fa4d8f60c
jeroencoessens <jc@jeroencoessens.com>
2016-10-31 15:47:12 +0100

    Initial commit
    Added some of the necessary files and project

---
+++ Secret of Mana/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterManager {
+
+    public List<Character.PlayerCharacter> CharacterList = new List<Character.PlayerCharacter>();
+    public List<Character.EnemyCharacter> EnemyList = new List<Character.EnemyCharacter>();
+
+    public Character.PlayerCharacter SelectedCharacter;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKeyDown(KeyCode.Alpha1))
+	    {
+	        SelectedCharacter = CharacterList[0];
+	    }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SelectedCharacter = CharacterList[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SelectedCharacter = CharacterList[2];
+        }
+    }
+}
---
+++ Secret of Mana/Assets/Scripts/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory {
+
+    public List<Item> ItemList = new List<Item>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
